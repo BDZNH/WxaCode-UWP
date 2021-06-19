27cf83ead3a8362c25d0c79907552f4746ee92d1
BDZNH <pxezhsp@live.com>
2021-06-19 21:51:59 +0800

    添加项目文件。

---
+++ WxaCode/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using Newtonsoft.Json.Linq;
+using Windows.Storage.Streams;
+using Windows.Storage;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace WxaCode
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        WxaCode wxaCode;
+        static byte[] data;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void GetWxaCodeUnlimited(object sender, RoutedEventArgs e)
+        {
+            wxacodeimage.Source = new BitmapImage(new Uri("ms-appx:///Assets/Loading.png"));
+            Button button = sender as Button;
+            button.IsEnabled = false;
+            if (wxaCode==null)
+            {
+                wxaCode = new WxaCode(appid.Text, appsecret.Text);
+            }
+            data = null;
+            data = wxaCode.getWxaCode(getwxacodeParam(), false);
+            if(data!=null)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                using(InMemoryRandomAccessStream stream = new InMemoryRandomAccessStream())
+                {
+                    await stream.WriteAsync(data.AsBuffer());
+                    stream.Seek(0);
+                    await bitmapImage.SetSourceAsync(stream);
+                    wxacodeimage.Source = bitmapImage;
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog(wxaCode.lasstErrorMsg, "好像发生了一些错误");
+                dialog.Commands.Add(new UICommand("确定", cmd => { }, commandId: 0));
+                await dialog.ShowAsync();
+            }
+            button.IsEnabled = true;
+        }
+
+        private void ToggleAutoColorSwitch(object sender, RoutedEventArgs e)
+        {
+            if(auto_color!=null)
+            {
+                if(auto_color.IsOn==true)
+                {
+                    if(line_corlor_red!=null)
+                    {
+                        line_corlor_red.Value = 0;
+                    }
+
+                    if(line_corlor_green!=null)
+                    {
+                        line_corlor_green.Value = 0;
+                    }
+                    
+                    if(line_corlor_blue!=null)
+                    {
+                        line_corlor_blue.Value = 0;
+                    }
+                }
+            }
+        }
+
+        private string getwxacodeParam()
+        {
+            JObject jobject = new JObject();
+            jobject.Add("scene", scene.Text);
+            jobject.Add("width", (int)wxacode_width.Value);
+            jobject.Add("auto_color", auto_color.IsOn);
+            if (auto_color.IsOn == false)
+            {
+                JObject linecolor = new JObject();
+                linecolor.Add("r", (int)line_corlor_red.Value);
+                linecolor.Add("g", (int)line_corlor_red.Value);
+                linecolor.Add("b", (int)line_corlor_red.Value);
+                jobject.Add("line_color", linecolor);
+            }
+            jobject.Add("is_hyaline", is_hyaline.IsOn);
+            return jobject.ToString();
+        }
+
+        private async void SaveWxacodeImage(object sender, RoutedEventArgs e)
+        {
+            if (data != null)
+            {
+                var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+                savePicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+                savePicker.FileTypeChoices.Add("Image", new List<string>() { ".jpg" });
+                savePicker.SuggestedFileName = "WxaCode";
+                Windows.Storage.StorageFile file = await savePicker.PickSaveFileAsync();
+                if (file != null)
+                {
+                    await FileIO.WriteBytesAsync(file, data);
+                    Windows.Storage.Provider.FileUpdateStatus status = await Windows.Storage.CachedFileManager.CompleteUpdatesAsync(file);
+                    if (status == Windows.Storage.Provider.FileUpdateStatus.Complete)
+                    {
+                        var dialog = new MessageDialog("保存成功", "保存小程序码");
+                        dialog.Commands.Add(new UICommand("确定", cmd => { }, commandId: 0));
+                        await dialog.ShowAsync();
+                    }
+                    else
+                    {
+                        var dialog = new MessageDialog("保存失败", "保存小程序码");
+                        dialog.Commands.Add(new UICommand("确定", cmd => { }, commandId: 0));
+                        await dialog.ShowAsync();
+                    }
+                }
+            }
+            else
+            {
+                var dialog = new MessageDialog("您还没有获取过小程序码", "保存小程序码");
+                dialog.Commands.Add(new UICommand("确定", cmd => { }, commandId: 0));
+                await dialog.ShowAsync();
+            }
+        }
+    }
+}
---
+++ WxaCode/WxaCode.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace WxaCode
+{
+    class WxaCode
+    {
+        private string appid;
+        private string appsecret;
+        private string accessToken;
+        public string lasstErrorMsg;
+        public WxaCode(string _appid,string _appsecret)
+        {
+            appid = _appid;
+            appsecret = _appsecret;
+        }
+
+        public WxaCode(string _appid, string _appsecret,bool _tmpwxacode)
+        {
+            //ignore
+        }
+
+        public string refreshAccessToken()
+        {
+            accessToken = null;
+            StringBuilder url = new StringBuilder(512);
+            url.Append(@"https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=");
+            url.Append(appid);
+            url.Append(@"&secret=");
+            url.Append(appsecret);
+
+            List<Byte> data = handlerHttpsCallBack(url.ToString(), false, null);
+            if(data!=null)
+            {
+                byte[] response = data.ToArray();
+                string tmpresponse = System.Text.Encoding.ASCII.GetString(response);
+                if (tmpresponse.Contains("access_token"))
+                {
+                    JObject jsondata = JsonConvert.DeserializeObject<JObject>(tmpresponse);
+                    accessToken = jsondata.GetValue("access_token").ToString();
+                    return accessToken;
+                }
+                else
+                {
+                    lasstErrorMsg = tmpresponse;
+                }
+            }
+            else
+            {
+                lasstErrorMsg = "maybe there are net work error";
+            }
+
+            return null;
+        }
+
+        public bool equals(string _appid, string _appsecret)
+        {
+            if(appid.Equals(_appid) && appsecret.Equals(_appsecret))
+            {
+                return true;
+            }
+            else
+            {
+                appid = _appid;
+                appsecret = _appsecret;
+                return false;
+            }
+        }
+
+        public byte[] getWxaCode(String wxacodeparams,Boolean refreshAccesstoken)
+        {
+            if(refreshAccesstoken)
+            {
+                accessToken = refreshAccessToken();
+                if(accessToken==null)
+                {
+                    // maybe network error or appid/appsecret error
+                    return null;
+                }
+            }
+            string url = @"https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=";
+            url += accessToken;
+            List<byte> data = handlerHttpsCallBack(url, true, wxacodeparams);
+            if(data!=null)
+            {
+                byte[] response = data.ToArray();
+                if(response.Length>1024)
+                {
+                    return response;
+                }
+                else
+                {
+                    lasstErrorMsg = System.Text.Encoding.ASCII.GetString(response);
+                }
+            }
+            return null;
+        }
+
+        private List<byte> handlerHttpsCallBack(string url,bool post,string wxacodeparams)
+        {
+            var data = new List<byte>();
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            if (post)
+            {
+                request.ContentType = "application/json";
+                request.Method = "POST";
+                using(var writer = new System.IO.StreamWriter(request.GetRequestStream()))
+                {
+                    writer.Write(wxacodeparams);
+                    writer.Flush();
+                    writer.Close();
+                }
+            }
+            else
+            {
+                request.Method = "GET";
+            }
+
+            using (var response = request.GetResponse().GetResponseStream())
+            {
+                {
+                    var buffer = new byte[1024];
+
+                    long totalbytes = 0;
+                    int readbytes = 0;
+                    while ((readbytes = response.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        totalbytes += readbytes;
+                        for (int i = 0; i < readbytes; i++)
+                        {
+                            data.Add(buffer[i]);
+                        }
+                    }
+                }
+                return data;
+            }
+        }
+    }
+}
